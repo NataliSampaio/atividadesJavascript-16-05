9543cdab2833421fd10e7bf4a048c5680190daa4
natali <natalisampaio02@outlook.com>
2022-05-16 09:03:43 -0300

    Primeira versão

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function ativ_01_btn_01(){
+    window.alert("Você precionou o botão 1!");
+}
+
+function ativ_01_btn_02(){
+    window.alert("Você precionou o botão 2!");
+}
+
+function ativ_01_btn_03(){
+    window.alert("Você precionou o botão 3!");
+}
